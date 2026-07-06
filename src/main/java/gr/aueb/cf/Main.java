e9f4db4a7b46e00b4695e0020409ad04ed2ba002
makisragkousis <makisragkousis@gmail.com>
2026-07-06 12:38:30 +0300

    feat: add entity manager and entity transaction

--- src/main/java/gr/aueb/cf/Main.java
+++ src/main/java/gr/aueb/cf/Main.java
@@ -1,10 +1,31 @@
 package gr.aueb.cf;
 
-/**
- * Hello world!
- */
+import gr.aueb.cf.model.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 public class Main {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("schoolPU");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+
+            Teacher alice = new Teacher("Alice", "Smith");
+            em.persist(alice);
+
+            tx.commit();
+        } catch (RuntimeException e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
